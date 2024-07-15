9393fbc6bc2a6307f16f83833b0ae607f234ca88
kero-edward <kyrillos.edward17@gmail.com>
2024-07-15 20:00:22 +0300

    polymorphism via overloading

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
+
+            /// 
+            multiply(5, 2, 1);
+
             
         }
+
+        #region polymorphism via overloading
+        static void multiply(int number_1, int number_2)
+        {
+            Console.WriteLine($"{number_1} * {number_2} = {number_1 * number_2}");
+        }
+
+        static void multiply(int number_1, int number_2, int number_3)
+        {
+            Console.WriteLine($"{number_1} * {number_2} * {number_3} = {number_1 * number_2 * number_3}");
+        }
+
+        static void multiply(int number_1, int number_2, int number_3, int number_4)
+        {
+            Console.WriteLine($"{number_1} * {number_2} * {number_3} * {number_4} = {number_1 * number_2 * number_3 * number_4}");
+        }
+        #endregion
     }
 }
